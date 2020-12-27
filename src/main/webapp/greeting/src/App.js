1c3acd4d38456cf809e99e2366073d992c09ad44
Matt Werth <matthewwerth@gmail.com>
2020-12-27 12:34:26 -0500

    Data Coming Through To React

--- src/main/webapp/greeting/src/App.js
+++ src/main/webapp/greeting/src/App.js
@@ -1,11 +1,27 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios'
 import NavigationBar from './components/NavigationBar'
+
+const Greeting = () => {
+
+  const fetchGreeting = () => {
+    axios.get("http://localhost:8081/greeting").then(res=> {
+      console.log(res);
+    })
+  }
+
+  useEffect(()=> {
+    fetchGreeting()
+  }, [])
+
+  return <h1>Hello</h1>
+}
 
 function App() {
   return (
     <div className="App">
-        <NavigationBar/>
-        <p>Welcome!</p>
+      <Greeting/>
     </div>
   );
 }
