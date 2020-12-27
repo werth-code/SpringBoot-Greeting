b99f0206a35fd1b88a31e1e96d6b0985b6c3c4f3
Matt Werth <matthewwerth@gmail.com>
2020-12-27 08:19:41 -0500

    Initial Commit

---
+++ src/test/java/com/werth/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werth.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
