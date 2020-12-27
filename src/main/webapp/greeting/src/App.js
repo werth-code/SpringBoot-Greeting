dd7759b0c3ea5298dd92a0043819f6916967a5ca
Matt Werth <matthewwerth@gmail.com>
2020-12-27 09:54:44 -0500

    Added Navbar

--- src/main/webapp/greeting/src/App.js
+++ src/main/webapp/greeting/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar'
 
 function App() {
   return (
     <div className="App">
-
+        <NavigationBar/>
+        <p>Welcome!</p>
     </div>
   );
 }
---
+++ src/main/webapp/greeting/src/components/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+    render() {
+        return (<div>Navigation</div>);
+    }
+}
+
+export default NavigationBar;
