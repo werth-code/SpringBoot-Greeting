c8246d1674304ed3b76e65b6074604b229ac2c23
Matt Werth <matthewwerth@gmail.com>
2020-12-28 09:07:14 -0500

    Added Seperate File For GreetLogic

--- src/main/webapp/greeting/src/App.js
+++ src/main/webapp/greeting/src/App.js
@@ -1,32 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import axios from 'axios'
-
-const Greeting = () => {
-
-  const [greeting, setGreeting] = useState([]);
-  const [name, nameSetter] = useState("");
-
-  console.log("useState : " + useState(1))
-
-  const fetchGreeting = () => {
-    axios.get("http://localhost:8081/greeting").then(res => {
-      setGreeting(res.data.content)
-    })
-  }
-
-  useEffect(() => {
-    console.log('running')
-    fetchGreeting()
-  }, [])
-
-return <div>
-  <h1 id="greet">{greeting}</h1>
-  <input placeholder="Name" id="val"/>
-  <button type="submit" onClick={()=>nameSetter(document.getElementById("greet").innerHTML = "Hello, " + document.getElementById("val").value  + "!")}>submit</button>
-</div>
-
-}
+import React from 'react'
+import Greeting from './components/GreetLogic'
 
 function App() { //<Greeting/> returns our function above -
   return (
